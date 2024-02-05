5f2401af064824ae77cc71c1c3a604e9df3446fb
Prashant <prashant.gyawali123@gmail.com>
2024-02-05 23:59:04 +0545

    refactor: removed unnecessary code

--- src/components/ScreenshotModal/ScreenshotModal.jsx
+++ src/components/ScreenshotModal/ScreenshotModal.jsx
@@ -1,14 +1,7 @@
-import { memo, useCallback, useContext, useEffect, useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import checkIcon from  "../../assets/checkIcon.svg"
-import copyButton from "../../assets/copyButton.svg"
-import shareIcon from "../../assets/shareIcon.svg"
+import { memo, useCallback, useState } from 'react';
 import screenshotIcon from "../../assets/screenshotIcon.svg"
 
 import ModalComponent from "./ScreenshotComponent"
-
-
 
 
 
@@ -18,23 +11,6 @@ function ScreenshotModal() {
 
     const handleClose = useCallback(() => setShow(false));
     const handleShow = useCallback(() => setShow(true));
-
-    const [copied, setCopied] = useState(false);
-
-
-
-    const copyToClipboard = () => {
-        navigator.clipboard.writeText(`https://codewrite-2.vercel.app/shared/${code.type}/${code.sharedURL}`);
-        setCopied(true);
-    }
-
-
-    useEffect(() => {
-        if (copied) {
-            setTimeout(() => { setCopied(false) }, 2000);
-        }
-    }, [copied]);
-
 
     return (
         <>
