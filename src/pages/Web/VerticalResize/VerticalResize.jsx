a70f7574617fb25097c79308553b60cbff424992
Prashant <prashant.gyawali123@gmail.com>
2024-02-05 23:51:10 +0545

    fix: vertical resize cursor

--- src/pages/Web/VerticalResize/VerticalResize.jsx
+++ src/pages/Web/VerticalResize/VerticalResize.jsx
@@ -1,24 +1,27 @@
-import { memo, useCallback } from "react";
+import { memo, useCallback,useState } from "react";
 import "./VerticalResize.css";
 
 const VerticalResize = ({ setEditorHeight, editorContainerRef }) => {
+
+    const [resizing, setResizing] = useState(false);
+
     const handleResize = useCallback((event) => {
+        setResizing(true);
         event.stopPropagation();
         document.body.style.userSelect = "none";
+        document.body.style.cursor = "row-resize";
 
         const settingHeight = (e) => {
             e.stopPropagation();
-            let t =
-                editorContainerRef.current.getBoundingClientRect().height + (e.type === "touchmove" ? e.touches[0].clientY : e.clientY) -editorContainerRef.current.getBoundingClientRect().bottom + 12;
+            let t =editorContainerRef.current.getBoundingClientRect().height + (e.type === "touchmove" ? e.touches[0].clientY : e.clientY) -editorContainerRef.current.getBoundingClientRect().bottom + 8;
             setEditorHeight(t);
         };
 
         const endResize = () => {
-            removeEventListener(
-                event.type === "touchstart" ? "touchmove" : "mousemove",
-                settingHeight
-            );
+            removeEventListener(event.type === "touchstart" ? "touchmove" : "mousemove",settingHeight);
             document.body.style.userSelect = "auto";
+            document.body.style.cursor = "auto";
+            setResizing(false);
         };
 
         addEventListener(event.type === "touchstart" ? "touchmove" : "mousemove",settingHeight);
@@ -30,7 +33,7 @@ const VerticalResize = ({ setEditorHeight, editorContainerRef }) => {
     });
 
     return (
-        <div className="vertical-resize"
+        <div className={"vertical-resize" + (resizing ? " vertical-resize-active" : "")}
             onMouseDown={handleResize}
             onTouchStart={handleResize}
         ></div>
