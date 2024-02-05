997ba4ec0603413ae73179d3e2742a8c98b69e94
Prashant <prashant.gyawali123@gmail.com>
2024-02-05 23:49:00 +0545

    style: improved home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,38 +1,65 @@
 import React from 'react'
 
+function ProjectCards()
+{
+  return <div className='p-2'>Project</div>
+}
+
 export default function Home() {
   return (
     <>
-        <div>Simplistic Editor for the web</div>
-        <div>Brainstorm ideas and quickly bring them to life</div>
-        <div>
-          <div>
-            Simple and customizable
+      <div className='text-center'>
+        <h3>Simplistic Editor for the web</h3>
+        <h4>Brainstorm ideas and quickly bring them to life</h4>
+      </div>
+
+        <div className="d-flex justify-content-between container-fluid">
+          <div className='p-2 m-1'>
+            <div className="h4">Simple and customizable</div>
             <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum vel exercitationem corporis distinctio deleniti!</div>
           </div>
           <div>Scrolling sample photos of different idea themes</div>
         </div>
-        <div>
-          <div>
-            Realtime Preview
+        <div className="d-flex justify-content-between container-fluid">
+        <div className='p-2 m-1'>
+            <div className='h4'>Realtime Preview</div>
             <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum vel exercitationem corporis distinctio deleniti!</div>
           </div>
           <div>Scrolling sample photos of different ide themes</div>
         </div>
-        <div>
-          <div>
-            Simple and customizable
+        <div className="d-flex justify-content-between container-fluid">
+        <div className='p-2 m-1'>
+            <div className='h4'>Simple and customizable</div>
             <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum vel exercitationem corporis distinctio deleniti!</div>
           </div>
           <div>Scrolling sample photos of resized</div>
         </div>
-        <div>
-          <div>
-            Simple and customizable
+        <div className="d-flex justify-content-between container-fluid">
+        <div className='p-2 m-1'>
+            <div className='h4'>Simple and customizable</div>
             <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum vel exercitationem corporis distinctio deleniti!</div>
           </div>
           <div>Scrolling sample photos of resized</div>
         </div>
+
+
+        <section className='container-fluid'> 
+            <h4 className='text-center'>Discover</h4>
+            <div className='d-flex flex-wrap '>
+              <ProjectCards/>
+              <ProjectCards/>
+              <ProjectCards/>
+              <ProjectCards/>
+              <ProjectCards/>
+              <ProjectCards/>
+              <ProjectCards/>
+              <ProjectCards/>              
+              <ProjectCards/>
+              <ProjectCards/>
+              <ProjectCards/>
+              <ProjectCards/>
+            </div>
+        </section>
     </>
   )
 }
