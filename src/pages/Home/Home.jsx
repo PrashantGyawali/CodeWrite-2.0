5c682c72a6b796a1d806776b337e50eecd71bab9
Prashant <prashant.gyawali123@gmail.com>
2024-02-20 20:26:12 +0545

    Home Page Update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Home.css"
 
 function ProjectCards()
 {
@@ -8,34 +9,51 @@ function ProjectCards()
 export default function Home() {
   return (
     <>
-      <div className='text-center'>
-        <h3>Simplistic Editor for the web</h3>
-        <h4>Brainstorm ideas and quickly bring them to life</h4>
+
+
+      <section className='hero-section'>
+      <div className='text-center hero-image'>
       </div>
+        <div className='hero-text'>
+          <div className='hero-title h1'>Simplistic Editor for the web</div>
+          <div className='hero-subtitle h2'>Brainstorm ideas and quickly bring them to life</div>
+        </div>
+      </section>
+
+
+
+        <div className="d-flex justify-content-between container-fluid">
+
+          <div className='container m-1'>
+            <div className="h4">Simple and customizable</div>
+            <div>Explore the simple yet fully customizable editor for web developers right in your browser!</div>
+          </div>
+
+
+          <div>Scrolling sample photos of different idea themes</div>
+
+        </div>
 
         <div className="d-flex justify-content-between container-fluid">
           <div className='p-2 m-1'>
-            <div className="h4">Simple and customizable</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum vel exercitationem corporis distinctio deleniti!</div>
-          </div>
-          <div>Scrolling sample photos of different idea themes</div>
-        </div>
-        <div className="d-flex justify-content-between container-fluid">
-        <div className='p-2 m-1'>
             <div className='h4'>Realtime Preview</div>
             <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum vel exercitationem corporis distinctio deleniti!</div>
           </div>
           <div>Scrolling sample photos of different ide themes</div>
         </div>
+
+
         <div className="d-flex justify-content-between container-fluid">
-        <div className='p-2 m-1'>
+          <div className='p-2 m-1'>
             <div className='h4'>Simple and customizable</div>
             <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum vel exercitationem corporis distinctio deleniti!</div>
           </div>
           <div>Scrolling sample photos of resized</div>
         </div>
+
+        
         <div className="d-flex justify-content-between container-fluid">
-        <div className='p-2 m-1'>
+          <div className='p-2 m-1'>
             <div className='h4'>Simple and customizable</div>
             <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum vel exercitationem corporis distinctio deleniti!</div>
           </div>
