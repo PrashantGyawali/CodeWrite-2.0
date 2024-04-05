baa9f896c8c61b4ce57e352873ed89972e7f35da
Prashant <prashant.gyawali123@gmail.com>
2024-04-05 11:18:14 +0545

    Style: fixed md preview style

--- src/pages/Projects/MdProjectPreview.jsx
+++ src/pages/Projects/MdProjectPreview.jsx
@@ -2,7 +2,7 @@ import MarkdownOutput from '../Markdown/MarkdownOutput'
 
 export default function MdProjectPreview({projectInfo, navigate}) {
     return (
-    <div onClick={()=>navigate(`/self/md/${projectInfo.id}`)} className='cursor-pointer w-100 pe-3 py-1'>
+    <div onClick={()=>navigate(`/self/md/${projectInfo.id}`)} className='md-project cursor-pointer pe-3 py-1'>
         <div className=' p-2  border-radius-10 subtle-border bg-darker'  style={{width:"100%",height:"40vh",overflow:"scroll",scrollbarWidth:"none",msOverflowStyle: "none"}}>
         <MarkdownOutput markdown={projectInfo.md || "## Empty Project 😱 ... \n\n Nothing to show here 🥴"} />
         </div>
